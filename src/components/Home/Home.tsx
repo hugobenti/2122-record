97e2dacaf31d5b7b24a605f7c7e5becc69dfd71d
Hugo Bentivegna <hugobentivegna@MacBook-Air-de-Hugo.local>
2024-10-17 11:02:24 -0300

    home youtube video

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 import AnimatePolvo from "./components/AnimatePolvo";
+import YoutubeEmbed from "../YoutubeEmbed";
 
 const Home = () => {
   const { homeRef, animations } = useAppContext();
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="flex w-full justify-between gap-8 items-center">
         <div className="h-full flex flex-col gap-8 items-center justify-center">
           <div className="w-full pb-6" ref={homeRef} id="home">
-            <p
+            {/* <p
               className={`text-stone-400 large-title transition-all delay-500 duration-1000 ease-in overflow-hidden home-text ${
                 animations.home
                   ? "opacity-100 max-w-[720px]"
@@ -19,13 +20,22 @@ const Home = () => {
             >
               Somos uma equipe dedicada a oferecer soluções inovadoras e
               personalizadas para o seu negócio.
-            </p>
-          </div>
-          <div className="rounded-lg text-center text-stone-400 p-6 bg-stone-800">
-            Fale com a gente
+            </p> */}
+            <div className="flex h-full">
+              <div className="w-6 h-auto bg-stone-700" />
+              <YoutubeEmbed embedId="IwQ2eFQwdoE" />
+              <div className="w-6 h-auto bg-stone-700" />
+            </div>
           </div>
         </div>
-        <AnimatePolvo />
+        <div className="flex items-center justify-center flex-col transition-all">
+          <div className="scale-[1.7]">
+            <AnimatePolvo />
+          </div>
+          <button className="z-10 group rounded-lg text-center text-stone-100 px-6 py-4 bg-stone-800 opacity-80 hover:opacity-100 transition-all hover:bg-stone-600 hover:text-stone-200 cursor-pointer">
+            Saiba mais
+          </button>
+        </div>
       </div>
     </div>
   );
